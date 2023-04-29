547ea6e9981a553706336aae72233baf9be703dc
Lilypad <hyuntaenyc@gmail.com>
2023-04-29 04:41:35 -0400

    unittest

--- test.js
+++ test.js
@@ -1,6 +1,15 @@
 function Greet(name){
   if(name === "Elizabeth"){ 
-  return console.log("Hello there!"); 
-  };
+  return console.log(`Hello ${name}!`); 
+  }
+  else{
+  return console.log("Hello there!");
+  }
+
+  if(name === name.toUppercase()){
+  return console.log(`HELLO ${name}!`);
+  }
+
+
 }
 
